a089d77523b9d412ed49fa7a5ac1e5fd6027efde
kilianBertholon <kilian.bertholon02@gmail.com>
2024-01-17 14:41:16 +0100

    Initialisation du projet

---
+++ server.R
@@ -0,0 +1,7 @@
+###Library
+library(shiny)
+library(shinydashboard)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+}
---
+++ ui.R
@@ -0,0 +1,13 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+
+###Library
+library(shiny)
+library(shinydashboard)
+
+dashboardPage(
+  dashboardHeader(title = "Application de Monitoring"),
+  dashboardSidebar(),
+  dashboardBody()
+)
