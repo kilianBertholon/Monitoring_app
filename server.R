11f9a6baf6e5e76ca6987db14f519b8adc586a80
kilianBertholon <kilian.bertholon02@gmail.com>
2024-01-19 18:31:48 +0100

    Ajout de toutes les graphes

--- server.R
+++ server.R
@@ -76,78 +76,288 @@ server <- function(input, output, session) {
   
   ### Partie Performances 
   
-  output$graph_CMJ_FC_recup_IFT30_15_SJ <- renderPlotly({
+  # Fonction pour créer un graphique plotly avec des lignes reliant les points
+  # Fonction pour créer un graphique plotly avec des lignes reliant les points de chaque athlète
+  create_plotly_chart <- function(data, variable_name, title, highlight_color) {
     sujet_select <- input$Sujet
-    date_select <- input$Date
-    
-    # Vérifier si un sujet est sélectionné
-    if (!is.null(sujet_select)) {
-      data_sujet <- data_num[data_num$Sujet == sujet_select, ]
-      
-      # Vérifier si des lignes sont présentes pour le sujet sélectionné
-      if (nrow(data_sujet) > 0) {
-        # Filtrer par date si une date est sélectionnée
-        if (!is.null(date_select)) {
-          data_CMJ <- data_sujet %>%
-            filter(Variable == "CMJ", Date %in% date_select) %>%
-            arrange(desc(Date))
-          data_FC_recup <- data_sujet %>%
-            filter(Variable == "FC_recup", Date %in% date_select) %>%
-            arrange(desc(Date))
-          data_IFT30_15 <- data_sujet %>%
-            filter(Variable == "IFT30_15", Date %in% date_select) %>%
-            arrange(desc(Date))
-          data_SJ <- data_sujet %>%
-            filter(Variable == "SJ", Date %in% date_select) %>%
-            arrange(desc(Date))
-        } else {
-          # Si aucune date n'est sélectionnée, prendre toutes les dates
-          data_CMJ <- data_sujet %>%
-            filter(Variable == "CMJ") %>%
-            arrange(desc(Date))
-          data_FC_recup <- data_sujet %>%
-            filter(Variable == "FC_recup") %>%
-            arrange(desc(Date))
-          data_IFT30_15 <- data_sujet %>%
-            filter(Variable == "IFT30_15") %>%
-            arrange(desc(Date))
-          data_SJ <- data_sujet %>%
-            filter(Variable == "SJ") %>%
-            arrange(desc(Date))
-        }
-        
-        # Créer les graphiques plotly et les afficher côte à côte
-        p1 <- plot_ly(data_CMJ, x = ~Date, y = ~Valeur, type = "scatter", mode = "markers") %>%
-          layout(title = "Sauts CMJ par date",
-                 xaxis = list(title = "Date"),
-                 yaxis = list(title = "Hauteur du saut"))
-        
-        p2 <- plot_ly(data_FC_recup, x = ~Date, y = ~Valeur, type = "scatter", mode = "markers") %>%
-          layout(title = "Fréquence cardiaque de récupération par date",
-                 xaxis = list(title = "Date"),
-                 yaxis = list(title = "Fréquence cardiaque de récupération"))
-        
-        p3 <- plot_ly(data_IFT30_15, x = ~Date, y = ~Valeur, type = "scatter", mode = "markers") %>%
-          layout(title = "IFT30_15 par date",
-                 xaxis = list(title = "Date"),
-                 yaxis = list(title = "IFT30_15"))
-        
-        p4 <- plot_ly(data_SJ, x = ~Date, y = ~Valeur, type = "scatter", mode = "markers") %>%
-          layout(title = "Sauts SJ par date",
-                 xaxis = list(title = "Date"),
-                 yaxis = list(title = "Hauteur du saut"))
-        
-        subplot(p1, p2, p3, p4, nrows = 1)
-      } else {
-        # Aucune ligne pour le sujet sélectionné
-        return(NULL)
-      }
-    } else {
-      # Aucun sujet sélectionné
-      return(NULL)
-    }
-  })
+    data_sujet <- data[data$Sujet == sujet_select, ]
+    plot_ly(data, x = ~Date, y = ~Valeur, type = "scatter", mode = "markers",
+            marker = list(color = ~ifelse(data$Sujet == sujet_select, highlight_color, "gray")),
+            showlegend = FALSE) %>%
+      add_trace(data = data_sujet, type = "scatter", mode = "lines",
+                line = list(color = highlight_color),
+                inherit = FALSE) %>%
+      layout(title = title,
+             xaxis = list(title = "Date"),
+             yaxis = list(title = variable_name))
+  }
+
+# Observer event pour mettre à jour les graphiques
+observe({
+  sujet_select <- input$Sujet
+  date_select <- input$Date
   
+  # Vérifier si un sujet est sélectionné
+  if (!is.null(sujet_select)) {
+    data_sujet <- data_num
+    data_sujet$highlight_color <- ifelse(data_sujet$Sujet == sujet_select, "blue", "gray")
+    
+    # Filtrer par date si une date est sélectionnée
+    if (!is.null(date_select)) {
+      data_sujet <- data_sujet %>%
+        filter(Date %in% date_select) %>%
+        arrange(desc(Date))
+    } else {
+      # Si aucune date n'est sélectionnée, prendre toutes les dates
+      data_sujet <- data_sujet %>%
+        arrange(desc(Date))
+    }
+    
+    # Créer les graphiques plotly avec lignes reliant les points de chaque athlète
+    output$graph_CMJ <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "CMJ",],
+                                             "Hauteur du saut(cm)", "Sauts CMJ", "blue")
+    })
+    
+    output$graph_FC_recup <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "FC_recup",],
+                                             "Fréquence cardiaque de récupération (bpm)", "FC récup", "blue")
+    })
+    
+    output$graph_IFT30_15 <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "IFT30_15",],
+                                             "IFT30_15 (km/h)", "IFT30_15", "blue")
+    })
+    
+    output$graph_SJ <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "SJ",],
+                                             "Hauteur du saut (cm)", "Sauts SJ", "blue")
+    })
+    
+    output$graph_Azote_ureique <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Azote_ureique",],
+                          "Azote uréique", "Azote uréique", "blue")
+    })
+    
+    output$graph_Magnesium <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Magnesium",],
+                          "Magnesium", "Magnesium", "blue")
+    })
+    
+    output$graph_Bilirubine <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Bilirubine",],
+                          "Bilirubine", "Bilirubine", "blue")
+    })
+    
+    
+    output$Lactate_deshydrogenase <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Lactate_deshydrogenase",],
+                          "Lactate deshydrogenase", "Lactate deshydrogenase", "blue")
+    })
+    
+    output$Creatine_kinase <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Creatine_kinase",],
+                          "Creatine kinase", "Creatine kinase", "blue")
+    })
+    
+    output$Acide_Urique <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Acide_Urique",],
+                          "Acide Urique", "Acide Urique", "blue")
+    })
+    
+    output$Proteine_C_reactive <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Proteine_C_reactive",],
+                          "Proteine C reactive", "Proteine C reactive", "blue")
+    })
+    
+    output$Sodium <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Sodium",],
+                          "Sodium", "Sodium", "blue")
+    })
+    
+    output$Potassium <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Potassium",],
+                          "Potassium", "Potassium", "blue")
+    })
+    
+    output$Calcium <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Calcium",],
+                          "Calcium", "Calcium", "blue")
+    })
+    
+    output$Myoglobine <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Myoglobine",],
+                          "Myoglobine", "Myoglobine", "blue")
+    })
+    
+    output$Cholesterol <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Cholesterol",],
+                          "Cholesterol", "Cholesterol", "blue")
+    })
+    
+    output$HDL <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "HDL",],
+                          "HDL", "HDL", "blue")
+    })
+    
+    output$LDL <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "LDL",],
+                          "LDL", "LDL", "blue")
+    })
+    
+    output$Triglicerides <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Triglicerides",],
+                          "Triglicerides", "Triglicerides", "blue")
+    })
+    
+    output$Glucose <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Glucose",],
+                          "Glucose", "Glucose", "blue")
+    })
+    
+    output$WBC <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "WBC",],
+                          "WBC", "WBC", "blue")
+    })
+    
+    output$Neutrophiles <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Neutrophiles",],
+                          "Neutrophiles", "Neutrophiles", "blue")
+    })
+    
+    output$Lymphocytes <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Lymphocytes",],
+                          "Lymphocytes", "Lymphocytes", "blue")
+    })
+    
+    
+    output$Monocytes <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Monocytes",],
+                          "Monocytes", "Monocytes", "blue")
+    })
+    
+    output$Eosinophile <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Eosinophile",],
+                          "Eosinophile", "Eosinophile", "blue")
+    })
+    
+    output$Basophile <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Basophile",],
+                          "Basophile", "Basophile", "blue")
+    })
+    
+    output$Plaquettes <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Plaquettes",],
+                          "Plaquettes", "Plaquettes", "blue")
+    })
+    
+    output$RBC <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "RBC",],
+                          "RBC", "RBC", "blue")
+    })
+    
+    output$Hemoglobine <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Hemoglobine",],
+                          "Hemoglobine", "Hemoglobine", "blue")
+    })
+    
+    output$MCV <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "MCV",],
+                          "MCV", "MCV", "blue")
+    })
+    
+    output$Hematocrite <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Hematocrite",],
+                          "Hematocrite", "Hematocrite", "blue")
+    })
+    
+    output$MCH <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "MCH",],
+                          "MCH", "MCH", "blue")
+    })
+    
+    output$MCHC <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "MCHC",],
+                          "MCHC", "MCHC", "blue")
+    })
+    
+    output$Transferrine <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Transferrine",],
+                          "Transferrine", "Transferrine", "blue")
+    })
+    
+    output$Ferritine <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Ferritine",],
+                          "Ferritine", "Ferritine", "blue")
+    })
+    
+    output$Fer <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Fer",],
+                          "Fer", "Fer", "blue")
+    })
+    
+    output$Sat_transferrine <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Sat_transferrine",],
+                          "Saturation en transferrine", "Saturation en transferrine", "blue")
+    })
+    
+    output$Testosterone <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Testosterone",],
+                          "Testosterone", "Testosterone", "blue")
+    })
+    
+    output$Cortisol <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Cortisol",],
+                          "Cortisol", "Cortisol", "blue")
+    })
+    
+    output$IL_6 <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "IL_6",],
+                          "IL_6", "IL_6", "blue")
+    })
+    
+    ##### Partie vitamines
+    output$Retinol <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Retinol",],
+                          "Retinol", "Retinol", "blue")
+    })
+    
+    output$Beta_carotene <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Beta_carotene",],
+                          "Beta carotene", "Beta carotene", "blue")
+    })
+    
+    output$Vit_E <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Vit_E",],
+                          "Vitamine E", "Vitamine E", "blue")
+    })
+    
+    output$Vit_B6 <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Vit_B6",],
+                          "Vitamine B6", "Vitramine B6", "blue")
+    })
+    
+    output$Vit_B12 <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Vit_B12",],
+                          "Vitamine B12", "Vitramine B12", "blue")
+    })
+    
+    output$Vit_C <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "Vit_C",],
+                          "Vitamine C", "Vitamine C", "blue")
+    })
+    
+    output$e1_25_dihydroxyvitamine_D <- renderPlotly({
+      create_plotly_chart(data_sujet[data_sujet$Variable == "1_25-dihydroxyvitamine_D",],
+                          "1_25_dihydroxyvitamine_D", "1_25_dihydroxyvitamine_D", "blue")
+    })
+   
+  } else {
+    # Aucun sujet sélectionné
+    return(NULL)
+  }
+})
+
   
   
   
--- ui.R
+++ ui.R
@@ -52,12 +52,245 @@ ui <- dashboardPage(
         box(width = 12, title = "Données anthropométriques",
             DTOutput("tableau_anth"))
       )),
+      fluidRow(column(width = 12, titlePanel(
+        h3("Performances", style = "color: #222d32; font-size: 40px; text-align: center;")
+      ))),
+      fluidRow(column(width = 12,
+                      fluidRow(
+                        box(
+                          width = 3,
+                          plotlyOutput("graph_CMJ", width = "100%", height = "400px")
+                        ),
+                        box(
+                          width = 3,
+                          plotlyOutput("graph_FC_recup", width = "100%", height = "400px")
+                        ),
+                        box(
+                          width = 3,
+                          plotlyOutput("graph_IFT30_15", width = "100%", height = "400px")
+                        ),
+                        box(
+                          width = 3,
+                          plotlyOutput("graph_SJ", width = "100%", height = "400px")
+                        )
+                      ))),
+      fluidRow(column(width = 12, titlePanel(
+        h3("Serum Sang", style = "color: #222d32; font-size: 40px; text-align: center;")
+      ))),
       fluidRow(column(
         width = 12,
-        box(width = 12, 
-            title = "Performances",
-            plotlyOutput("graph_CMJ_FC_recup_IFT30_15_SJ"))
+        fluidRow(
+          box(
+            width = 3,
+            plotlyOutput("graph_Azote_ureique", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("graph_Magnesium", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("graph_Bilirubine", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput(
+              "Lactate_deshydrogenase",
+              width = "100%",
+              height = "400px"
+            )
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Creatine_kinase", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Acide_Urique", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Proteine_C_reactive", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "Sodium", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Potassium", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "Calcium", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Myoglobine", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Cholesterol", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "HDL", width = "100%", height = "400px"
+              )),
+          box(width = 3,
+              plotlyOutput(
+                "LDL", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Triglicerides", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "Glucose", width = "100%", height = "400px"
+              ))
+        )
+      )),
+      fluidRow(column(width = 12, titlePanel(
+        h3("Analyse Sang totale", style = "color: #222d32; font-size: 40px; text-align: center;")
+      ))),
+      fluidRow(column(
+        width = 12,
+        fluidRow(
+          box(width = 3,
+              plotlyOutput(
+                "WBC", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Neutrophiles", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Lymphocytes", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Monocytes", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Eosinophile", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Basophile", width = "100%", height = "400px")
+          ),
+          box(
+            width = 3,
+            plotlyOutput("Plaquettes", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "RBC", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Hemoglobine", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "MCV", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Hematocrite", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "MCH", width = "100%", height = "400px"
+              )),
+          box(width = 3,
+              plotlyOutput(
+                "MCHC", width = "100%", height = "400px"
+              ))
+        )
+      )),
+      fluidRow(column(width = 12, titlePanel(
+        h3("Indice hémato-fer", style = "color: #222d32; font-size: 40px; text-align: center;")
+      ))),
+      fluidRow(column(width = 12,
+                      fluidRow(
+                        box(width = 3,
+                            plotlyOutput(
+                              "Fer", width = "100%", height = "400px"
+                            )),
+                        box(
+                          width = 3,
+                          plotlyOutput("Transferrine", width = "100%", height = "400px")
+                        ),
+                        box(
+                          width = 3,
+                          plotlyOutput("Ferritine", width = "100%", height = "400px")
+                        ),
+                        box(
+                          width = 3,
+                          plotlyOutput("Sat_transferrine", width = "100%", height = "400px")
+                        )
+                      ))),
+      fluidRow(column(width = 12, titlePanel(
+        h3("Hormones", style = "color: #222d32; font-size: 40px; text-align: center;")
+      ))),
+      fluidRow(column(width = 12,
+                      fluidRow(
+                        box(
+                          width = 4,
+                          plotlyOutput("Testosterone", width = "100%", height = "400px")
+                        ),
+                        box(
+                          width = 4,
+                          plotlyOutput("Cortisol", width = "100%", height = "400px")
+                        ),
+                        box(width = 4,
+                            plotlyOutput(
+                              "IL_6", width = "100%", height = "400px"
+                            ))
+                      ))),
+      fluidRow(column(width = 12, titlePanel(
+        h3("Vitamines", style = "color: #222d32; font-size: 40px; text-align: center;")
+      ))),
+      fluidRow(column(
+        width = 12,
+        fluidRow(
+          box(width = 3,
+              plotlyOutput(
+                "Retinol", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput("Beta_carotene", width = "100%", height = "400px")
+          ),
+          box(width = 3,
+              plotlyOutput(
+                "Vit_E", width = "100%", height = "400px"
+              )),
+          box(width = 3,
+              plotlyOutput(
+                "Vit_B6", width = "100%", height = "400px"
+              )),
+          box(width = 3,
+              plotlyOutput(
+                "Vit_B12", width = "100%", height = "400px"
+              )),
+          box(width = 3,
+              plotlyOutput(
+                "Vit_C", width = "100%", height = "400px"
+              )),
+          box(
+            width = 3,
+            plotlyOutput(
+              "e1_25_dihydroxyvitamine_D",
+              width = "100%",
+              height = "400px"
+            )
+          )
+        )
       ))
+      
     ),
     
     tabItem("donnee",
