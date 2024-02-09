1f60ae6355e4004bcc9c2e57a5c7e96fd232e2b7
kilianBertholon <kilian.bertholon02@gmail.com>
2024-02-09 23:40:37 +0100

    A telecharger

--- Import_data.R
+++ Import_data.R
@@ -1,6 +1,21 @@
 
 ################################# Range ######################################
+#Import data + traitement
+gs4_deauth()
+sheet_tidy <- "https://docs.google.com/spreadsheets/d/1jty0iGJY-FLLkEQHKyRe12qRD9MLZzEQd2TXjclGemM/edit#gid=0"
+data <- read_sheet(sheet_tidy)
 
+#Traitement 
+## Date
+data$Date <- format(data$Date, "%e/%m/%Y")
+
+#Data num (sans <,>)
+data_num <- copy(data)
+data_num$Valeur <- gsub(",", ".", data_num$Valeur)
+data_num$Valeur <- gsub("[<>]", "", data_num$Valeur)
+data_num$Valeur <- as.numeric(data_num$Valeur)
+data_num$Valeur <- round(data_num$Valeur, 3)
+data_num$Date <- as.Date(data_num$Date, format = "%d/%m/%Y")
 # Lire les données
 sheet_range <- "https://docs.google.com/spreadsheets/d/1ykAvHxprWWsUDyPJX5gTMTYV6vPJUm4dX-kc68UgiDw/edit?usp=sharing"
 range_value <- read_sheet(sheet_range)
@@ -37,19 +52,3 @@ data_radar$Date <- as.Date(data_radar$Date, format = "%d/%m/%Y")
 
 
 ############################# Tidy #########################################
-#Import data + traitement
-gs4_deauth()
-sheet_tidy <- "https://docs.google.com/spreadsheets/d/1jty0iGJY-FLLkEQHKyRe12qRD9MLZzEQd2TXjclGemM/edit#gid=0"
-data <- read_sheet(sheet_tidy)
-
-#Traitement 
-## Date
-data$Date <- format(data$Date, "%e/%m/%Y")
-
-#Data num (sans <,>)
-data_num <- copy(data)
-data_num$Valeur <- gsub(",", ".", data_num$Valeur)
-data_num$Valeur <- gsub("[<>]", "", data_num$Valeur)
-data_num$Valeur <- as.numeric(data_num$Valeur)
-data_num$Valeur <- round(data_num$Valeur, 3)
-data_num$Date <- as.Date(data_num$Date, format = "%d/%m/%Y")
